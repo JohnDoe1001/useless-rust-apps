096a59cfdef6f11474fb6bc0c815610a68b693a5
JohnDoe1001 <102891530+JohnDoe1001@users.noreply.github.com>
2023-10-05 12:54:44 +0700

    Create Fibonacci-sequence-generator.rs

---
+++ Fibonacci-sequence-generator.rs
@@ -0,0 +1,17 @@
+fn main() {
+  // A Fibonacci sequence generator.
+  // Algorithm: Fn = Fn - 1 + Fn - 2
+  // F = Number, n = location.
+  // Course of action:
+	// 0. Create a loop that loops for N
+  // times
+  // 1. Create two variables, named 
+	// a and b. Set a to 0, b to 1.
+	// 2. Add a and b together (0+1) 
+	// to form F (sum)
+	// 3. Set a to b, F to a.
+	// 4. Print them out.
+	// 5. Repeat.
+
+}
+  
